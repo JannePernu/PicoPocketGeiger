1976edc179baba102cf4c4a102397941aeccb1f8
Janne <55883062+JannePernu@users.noreply.github.com>
2023-02-23 09:08:49 +0200

    Update __init__.py
    The main changes to make it work on RaspPi Pico

--- PiPocketGeiger/__init__.py
+++ PiPocketGeiger/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Radiation Watch Pocket Geiger Type 5 library for Raspberry Pi.
+Radiation Watch Pocket Geiger Type 5 library for Raspberry Pi, modified for Pico.
 
 Documentation and usage at: https://github.com/MonsieurV/PiPocketGeiger
 
@@ -9,37 +9,31 @@ Released under MIT License. See LICENSE file.
 Contributed by:
 - Radiation-watch.org <http://www.radiation-watch.org/>
 - Yoan Tournade <yoan@ytotech.com>
-
-Source for wiring and pin readings:
-https://cdn.sparkfun.com/assets/learn_tutorials/1/4/3/GeigerCounterType5_connect_with_microcomputer.pdf
+- Janne Pernu <janne.pernu@protonmail.com>
 """
-import threading
+
+import _thread as threading
 import math
 import time
-import RPi.GPIO as GPIO
+from machine import Pin, Timer
 
 __all__ = ["RadiationWatch"]
 
 # Number of cells of the history array.
 HISTORY_LENGTH = 200
+
 # Duration of each history array cell (seconds).
 HISTORY_UNIT = 6
+
 # Process period for the statistics (milliseconds).
 PROCESS_PERIOD = 160
 MAX_CPM_TIME = HISTORY_LENGTH * HISTORY_UNIT * 1000
+
 # Magic calibration number from the Arduino lib.
 K_ALPHA = 53.032
 
-# Bounce delay during which we ignore further edges after an edge.
-# In ms.
-# See https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
-# We disable it, as we have no bouncing issues.
-BOUNCE_DELAY = None
-
-
 def millis():
-    """Return current time in milliseconds.
-    """
+    """Return current time in milliseconds."""
     return int(round(time.time() * 1000))
 
 
@@ -48,21 +42,21 @@ class RadiationWatch:
 
     Usage:
     ```
-    with RadiationWatch(24, 23) as radiationWatch:
+    with RadiationWatch(16, 15) as radiationWatch:
         # Do something with the lib.
         print(radiationWatch.status())
     ```
     """
 
-    def __init__(self, radiation_pin, noise_pin, numbering=GPIO.BCM):
+    def __init__(self, radiation_pin, noise_pin):
         """Initialize the Radiation Watch library, specifying the pin numbers
         for the radiation and noise pin.
         You can also specify the pin numbering mode (BCM numbering by
         default)."""
-        GPIO.setmode(numbering)
+        self.mutex = threading.allocate_lock()
+        
         self.radiation_pin = radiation_pin
         self.noise_pin = noise_pin
-        self.mutex = threading.Lock()
         self.radiation_callback = None
         self.noise_callback = None
 
@@ -72,8 +66,10 @@ class RadiationWatch:
             cpm -- the radiation count by minute;
             uSvh -- the radiation dose, expressed in microSieverts per hour (uSv/h);
             uSvhError -- the incertitude for the radiation dose."""
+        
         minutes = min(self.duration, MAX_CPM_TIME) / 1000 / 60.0
         cpm = self.count / minutes if minutes > 0 else 0
+        
         return dict(
             duration=round(self.duration / 1000.0, 2),
             cpm=round(cpm, 2),
@@ -84,11 +80,11 @@ class RadiationWatch:
         )
 
     def register_radiation_callback(self, callback):
-        """Register a function that will be called on radiation occurrence. """
+        """Register a function that will be called on radiation occurrence."""
         self.radiation_callback = callback
 
     def register_noise_callback(self, callback):
-        """Register a function that will be called on noise occurrence. """
+        """Register a function that will be called on noise occurrence."""
         self.noise_callback = callback
 
     def __enter__(self):
@@ -99,40 +95,36 @@ class RadiationWatch:
 
     def setup(self):
         """Initialize the driver by setting up GPIO interrupts
-        and periodic statistics processing. """
+        and periodic statistics processing."""
+        
         # Initialize the statistics variables.
         self.radiation_count = 0
         self.noise_count = 0
         self.count = 0
+        
         # Initialize count_history[].
         self.count_history = [0] * HISTORY_LENGTH
         self.history_index = 0
+        
         # Init measurement time.
         self.previous_time = millis()
         self.previous_history_time = millis()
         self.duration = 0
-        # Init the GPIO context.
+
         # Raw data of Radiation Pulse: Not-detected -> High, Detected -> Low.
-        GPIO.setup(self.radiation_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.radsPin = Pin(self.radiation_pin, Pin.IN, Pin.PULL_UP)
+        
         # Raw data of Noise Pulse: Not-detected -> Low, Detected -> High.
-        GPIO.setup(self.noise_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        self.noisePin = Pin(self.noise_pin, Pin.IN, Pin.PULL_DOWN)
+        
+        
         # Register local callbacks.
-        # As the signal from the radiation pin is high by default
-        # and low when a ray hit the sensor, we want to listen on the edges falls.
-        GPIO.add_event_detect(
-            self.radiation_pin,
-            GPIO.FALLING,
-            callback=self._on_radiation,
-            **{"bouncetime": BOUNCE_DELAY} if BOUNCE_DELAY else {},
-        )
-        # As the signal from the noise pin is low by default and high when noise occurs,
-        # we want to listen on the edges rises.
-        GPIO.add_event_detect(
-            self.noise_pin,
-            GPIO.RISING,
-            callback=self._on_noise,
-            **{"bouncetime": BOUNCE_DELAY} if BOUNCE_DELAY else {},
-        )
+        # Listen on the edges falls.
+        self.radsPin.irq(handler = self._on_radiation, trigger = Pin.IRQ_FALLING)
+        
+        # Listen on the edges rises.
+        self.noisePin.irq(handler = self._on_noise, trigger = Pin.IRQ_RISING)
+        
         # Enable the timer for processing the statistics periodically.
         self._enable_timer()
         return self
@@ -140,10 +132,14 @@ class RadiationWatch:
     def close(self):
         """Properly close the resources associated with the driver
         (GPIOs and so on)."""
-        # Clean up only used channels.
-        GPIO.cleanup([self.radiation_pin, self.noise_pin])
+        
+        # Clean up only used pins
+        self.radsPin= Pin(self.radiation_pin, Pin.OUT, Pin.low())
+        self.noisePin = Pin(self.noise_pin, Pin.OUT, Pin.low())
+        
         with self.mutex:
-            self.timer.cancel()
+            self.timer.deinit()
+
 
     def _on_radiation(self, _channel):
         with self.mutex:
@@ -158,49 +154,52 @@ class RadiationWatch:
             self.noise_callback()
 
     def _enable_timer(self):
-        self.timer = threading.Timer(PROCESS_PERIOD / 1000.0, self._process_statistics)
-        self.timer.start()
+        # Makes a timer and starts it. Default PROCESS_PERIOD = 160ms. Period is in ms
+        self.timer = Timer()
+        self.timer.init(mode = Timer.PERIODIC, period = PROCESS_PERIOD, callback = self._process_statistics)
 
-    def _process_statistics(self):
+    def _process_statistics(self, t):
+        #print("Enter _process_statistics")
         with self.mutex:
             current_time = millis()
+            #print(current_time)
             current_radiation_count = self.radiation_count
             current_noise_count = self.noise_count
             self.radiation_count = 0
             self.noise_count = 0
         if current_noise_count == 0:
-            # Store count log.
+            
+            # Store count log. Add number of counts. Add ellapsed time to history duration.
             self.count_history[self.history_index] += current_radiation_count
-            # Add number of counts.
             self.count += current_radiation_count
-            # Add ellapsed time to history duration.
             self.duration += abs(current_time - self.previous_time)
+        
         # Shift an array for counting log for each HISTORY_UNIT seconds.
         if current_time - self.previous_history_time >= HISTORY_UNIT * 1000:
             self.previous_history_time += HISTORY_UNIT * 1000
             self.history_index = (self.history_index + 1) % HISTORY_LENGTH
             self.count -= self.count_history[self.history_index]
             self.count_history[self.history_index] = 0
+
         # Save time of current process period.
         self.previous_time = millis()
+        
         # Enable the timer again.
-        if self.timer:
-            self._enable_timer()
 
+            
 
 if __name__ == "__main__":
-
     def on_radiation():
         """Test radiation event handler"""
-        print("Ray appeared!")
+        print("Radiation detected")
 
     def on_noise():
         """Test noise event handler"""
-        print("Vibration! Stop moving!")
+        print("Noise Detected: Ignore output")
 
-    with RadiationWatch(24, 23) as radiationWatch:
+    with RadiationWatch(27, 26) as radiationWatch:
         radiationWatch.register_radiation_callback(on_radiation)
         radiationWatch.register_noise_callback(on_noise)
         while 1:
             print(radiationWatch.status())
-            time.sleep(5)
+            time.sleep(0.5)
